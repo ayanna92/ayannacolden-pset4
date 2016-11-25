624b485e8871a14f9497cd0c24468f3f10d94870
ayanna92 <Ayanna@Ayannas-MacBook-Air.local>
2016-11-25 17:24:09 +0100

    finished todo list

---
+++ pset4/DatabaseHelper.swift
@@ -0,0 +1,93 @@
+//
+//  DatabaseHelper.swift
+//  pset4
+//
+//  Created by Ayanna Colden on 23/11/2016.
+//  Copyright © 2016 Ayanna Colden. All rights reserved.
+//
+
+import Foundation
+import SQLite 
+
+class DatabaseHelper {
+    
+    private let users = Table("users")
+    
+    private let id = Expression<Int64>("id")
+    private let todo = Expression<String?>("todo")
+    
+    private var db: Connection?
+    
+    init?() {
+        do {
+            try setupDatabase()
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    private func setupDatabase() throws {
+        
+        let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        
+        do {
+            db = try Connection("\(path)/db.sqlite3")
+            try createTable()
+        } catch {
+            throw error
+        }
+    }
+    
+    private func createTable() throws {
+        
+        do {
+            try db!.run(users.create(ifNotExists: true) {
+                t in
+                t.column(id, primaryKey: .autoincrement)
+                t.column(todo)
+            })
+        } catch {
+            throw error
+        }
+    }
+    
+    func create(todo: String) throws {
+        
+        let insert = users.insert(self.todo <- todo)
+        
+        do {
+            let rowId = try db!.run(insert)
+            print(rowId)
+        } catch {
+            throw error
+            
+        }
+    }
+    
+    func read() throws -> [String]{
+        var array = [String]()
+        
+        do {
+            
+            for user in try db!.prepare(users) {
+                array.insert(user[todo]!, at: 0)
+            }
+        } catch {
+            throw error
+        }
+        
+        return array
+    }
+    
+    func delete(task: String) throws {
+        let deletedRows = users.filter(todo == task)
+        
+        do {
+            try db!.run(deletedRows.delete())
+        } catch {
+            throw error
+        }
+    }
+    
+}
--- pset4/ViewController.swift
+++ pset4/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let db = DatabaseHelper()
+    @IBOutlet weak var todo: UITextField!
+    var array = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        
+        do {
+            array = try db!.read()
+            self.tableView.reloadData()
+            print(array)
+        } catch {
+            print(error)
+        }
+        
+        if db == nil {
+            print("Error")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,61 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func create(_ sender: Any) {
+        
+        DispatchQueue.main.async{
+            
+            do {
+                try self.db!.create(todo: self.todo.text!)
+            } catch {
+                print(error)
+                
+            }
+            
+            do {
+                self.array = try self.db!.read()
+                self.tableView.reloadData()
+                print(self.array)
+            } catch {
+                print(error)
+            }
+        }
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        
+        
+        cell.todoLabel.text = array[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            do {
+                try db!.delete(task: array.remove(at: indexPath.row))
+                print(array)
+            } catch {
+                print(error)
+            }
+            self.tableView.reloadData()
+        }
+    }
+    
 }
+
+
+
+
+
 
